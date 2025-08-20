1ba6792d0adbfc1638025458342f9df55ef3718a
Ramishwor sah <122091424+Ramishworsah@users.noreply.github.com>
2025-08-20 21:55:04 +0530

    done

--- app.py
+++ app.py
@@ -1,14 +1,2 @@
-from flask import Flask, request, render_template, jsonify
-import numpy as np
-import pickle
-import pandas as pd
-
-
-# creating flask app
-app=Flask(__name__)
-
-
-#python main
-
-if __name__=='__main__':
-    app.run(debug=True)
+import sklearn
+print(sklearn.__version__)
